dec1d2ced6867be012ac8188d074c5c2d6d937bb
Jose Orozco <jose816orozco@gmail.com>
2021-07-22 15:34:18 -0500

    worked on api

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Workout_Tracker
+# Workout Tracker ![MIT](https://img.shields.io/badge/license-MIT-blue.svg)
+
+![Screenshot](ScreenShot.png)
+
+# Description
+
+Created Mongo database with a Mongoose schema and handle routes with Express to use Workout Tracker.
+
+# Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributed](#contributed)
+- [License](#license)
+- [Questions](#questions)
+
+# Installation
+
+npm install
+
+# Usage
+
+Add and keep track of workouts.
+
+# Contributed
+
+Contributers: Jose Orozco
+
+# License
+
+This application is protected under conditions of the MIT license.
+
+# Questions
+
+My Github Profile: http://www.github.com/jose8160  
+ If there are any questions contact
+Jose816orozco@gmail.com
--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(logger("dev"))
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,13 +14,13 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
-app.use(require("./routes/api.js"));
+// app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on http://localhost:${PORT}!`);
+  console.log(`App running on http://localhost:${PORT}`);
 });
